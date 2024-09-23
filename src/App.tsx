a92e61c1dc718a968afd735f3e0965cc1aa4a305
ZhihuaSun1 <sunzhi@udel.edu>
2024-09-23 16:17:31 -0400

    modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
 import React from "react";
 import "./App.css";
 import { Button, Container, Row, Col } from "react-bootstrap";
-import { Counter } from "./components/Counter";
-import { ChangeType } from "./components/ChangeType";
-import { RevealAnswer } from "./components/RevealAnswer";
-import { StartAttempt } from "./components/StartAttempt";
-import { TwoDice } from "./components/TwoDice";
-import { CycleHoliday } from "./components/CycleHoliday";
-import { DoubleHalf } from "./bad-components/DoubleHalf";
-import { ColoredBox } from "./bad-components/ColoredBox";
-import { ShoveBox } from "./bad-components/ShoveBox";
-import { ChooseTeam } from "./bad-components/ChooseTeam";
 
 function App(): React.JSX.Element {
     return (
-        <>
+        <div className="App">
             <h1>
                 <span
                     style={{
@@ -25,33 +15,28 @@ function App(): React.JSX.Element {
                 >
                     MY FIRST HOMEWORK
                 </span>
-                <ol>
-                    <li>Get A</li>
-                    <li>Become a Software Engineer</li>
-                    <li>Study hard</li>
-                </ol>
-                <img src="public/logo192.png" alt="A picture" />
-                <div className="App">
-                    <header className="App-header">
-                        <Container>
-                            <Row>
-                                <Col>
-                                    UD CISC275 with React Hooks and TypeScript -
-                                    ZhihuaSun HHH Hello World
-                                </Col>
-                                <Col>Second part</Col>
-                            </Row>
-                        </Container>
-                    </header>
-
-                    <p>
-                        Edit <code>src/App.tsx</code> and save. This page will
-                        automatically reload.
-                    </p>
-                </div>
             </h1>
-
-            {}
+            <ol>
+                <li>Get A</li>
+                <li>Become a Software Engineer</li>
+                <li>Study hard</li>
+            </ol>
+            <img src="/logo192.png" alt="A picture" />
+            <header className="App-header">
+                <Container>
+                    <Row>
+                        <Col>
+                            UD CISC275 with React Hooks and TypeScript -
+                            ZhihuaSun HHH Hello World
+                        </Col>
+                        <Col>Second part</Col>
+                    </Row>
+                </Container>
+            </header>
+            <p>
+                Edit <code>src/App.tsx</code> and save. This page will
+                automatically reload.
+            </p>
             <Button
                 onClick={() => {
                     console.log("Hello World!");
@@ -59,30 +44,8 @@ function App(): React.JSX.Element {
             >
                 Log Hello World
             </Button>
-            {}
-            <hr></hr>
-            {<DoubleHalf></DoubleHalf>}
-            <hr></hr>
-            <ChooseTeam></ChooseTeam>
-            <hr></hr>
-            <ColoredBox></ColoredBox>
-            <hr></hr>
-            <ShoveBox></ShoveBox>
-            <hr></hr>
-            <Counter></Counter>
-            <hr />
-            <Counter />
-            <hr />
-            <ChangeType />
-            <hr />
-            <RevealAnswer />
-            <hr />
-            <StartAttempt />
-            <hr />
-            <TwoDice />
-            <hr />
-            <CycleHoliday />
-        </>
+        </div>
     );
 }
+
 export default App;
--- src/bad-components/DoubleHalfState.tsx
+++
@@ -1,4 +0,0 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-import { useState } from "react";
-
-export const [dhValue, setDhValue] = useState<number>(10);
