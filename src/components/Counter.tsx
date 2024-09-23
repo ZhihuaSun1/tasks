c7cd735f84061553a563eb27c3b9df01b6ec2459
ZhihuaSun1 <sunzhi@udel.edu>
2024-09-23 16:30:19 -0400

    modified:   src/components/ChangeType.tsx 	modified:   src/components/Counter.tsx 	modified:   src/components/CycleHoliday.tsx 	modified:   src/components/RevealAnswer.tsx 	modified:   src/components/StartAttempt.tsx 	modified:   src/components/TwoDice.tsx

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -3,10 +3,12 @@ import { Button } from "react-bootstrap";
 
 export function Counter(): React.JSX.Element {
     const [value, setValue] = useState<number>(0);
+
+    const incrementValue = () => { setValue(value + 1); };
+
     return (
         <span>
-            <Button onClick={() => setValue(1 + value)}>Add One</Button>
-            to {value}.
+            <Button onClick={incrementValue}>Add One</Button> to {value}.
         </span>
     );
 }
--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -1,16 +1,52 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
-/**
- * Here is a helper function you *must* use to "roll" your die.
- * The function uses the builtin `random` function of the `Math`
- * module (which returns a random decimal between 0 up until 1) in order
- * to produce a random integer between 1 and 6 (inclusive).
- */
 export function d6(): number {
     return 1 + Math.floor(Math.random() * 6);
 }
 
 export function TwoDice(): React.JSX.Element {
-    return <div>Two Dice</div>;
+    const generateDifferentDice = () => {
+        let left = d6();
+        let right = d6();
+        while (left === right) {
+            right = d6();
+        }
+        return { left, right };
+    };
+
+    const { left: initialLeft, right: initialRight } = generateDifferentDice();
+    const [leftDie, setLeftDie] = useState<number>(initialLeft);
+    const [rightDie, setRightDie] = useState<number>(initialRight);
+
+    // eslint-disable-next-line @typescript-eslint/no-confusing-void-expression
+    const rollLeftDie = () => setLeftDie(d6());
+    // eslint-disable-next-line @typescript-eslint/no-confusing-void-expression
+    const rollRightDie = () => setRightDie(d6());
+
+    const gameStatus = () => {
+        if (leftDie === 1 && rightDie === 1) {
+            return "Lose";
+        } else if (leftDie === rightDie) {
+            return "Win";
+        } else {
+            return null;
+        }
+    };
+    const statusMessage = gameStatus();
+
+    return (
+        <div>
+            <div>
+                <span data-testid="left-die">{leftDie}</span>
+                <span> vs </span>
+                <span data-testid="right-die">{rightDie}</span>
+            </div>
+
+            <Button onClick={rollLeftDie}>Roll Left</Button>
+            <Button onClick={rollRightDie}>Roll Right</Button>
+
+            {statusMessage && <div>{statusMessage}</div>}
+        </div>
+    );
 }
