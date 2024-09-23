66d3c092473e476a24988c14e5bc80fdd99c59d1
ZhihuaSun1 <sunzhi@udel.edu>
2024-09-23 14:03:02 -0400

    modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,6 @@ import { RevealAnswer } from "./components/RevealAnswer";
 import { StartAttempt } from "./components/StartAttempt";
 import { TwoDice } from "./components/TwoDice";
 import { CycleHoliday } from "./components/CycleHoliday";
-import { ColoredBox } from "./bad-components/ColoredBox";
-import { ShoveBox } from "./bad-components/ShoveBox";
-import { ChooseTeam } from "./bad-components/ChooseTeam";
 
 function App(): React.JSX.Element {
     return (
@@ -60,18 +57,6 @@ function App(): React.JSX.Element {
             </Button>
 
             {}
-            <hr>
-            {/* <DoubleHalf></DoubleHalf> */}
-            </hr>
-            <hr>
-            <ChooseTeam></ChooseTeam>
-            </hr>
-            <hr>
-            <ColoredBox></ColoredBox>
-            </hr>
-            <hr>
-            <ShoveBox></ShoveBox>
-            </hr>
             <hr />
             <Counter />
             <hr />
