1e5262977ad105bf31700fb0d7f2bfdcb6fe8d0c
ZhihuaSun1 <sunzhi@udel.edu>
2024-09-29 16:08:25 -0400

    1

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -5,8 +5,7 @@ export function d6(): number {
     return 1 + Math.floor(Math.random() * 6);
 }
 
-
-export function TwoDice(): JSX.Element {
+export function TwoDice(): React.ReactElement {
     const [d1, setD1] = useState<number>(1);
     const [d2, setD2] = useState<number>(5);
 
@@ -14,11 +13,11 @@ export function TwoDice(): JSX.Element {
         <div>
             <Button onClick={() => { setD1(d6()); }}>Roll Left</Button>
             <Button onClick={() => { setD2(d6()); }}>Roll Right</Button>
-            <br></br>
+            <br />
             <span data-testid="left-die">{d1}</span>
-            <br></br>
+            <br />
             <span data-testid="right-die">{d2}</span>
-            <br></br>
+            <br />
             {d1 === 1 && d2 === 1 ?
                 <span>Lose</span>
             : d1 === d2 && d1 !== 1 ?
