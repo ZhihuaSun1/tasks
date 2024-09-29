83b4bb3035644d12b53540a2af2ad9faabd6e921
ZhihuaSun1 <sunzhi@udel.edu>
2024-09-29 16:00:06 -0400

    1

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -5,58 +5,24 @@ export function d6(): number {
     return 1 + Math.floor(Math.random() * 6);
 }
 
-export function TwoDice(): React.JSX.Element {
-    const generateDifferentDice = () => {
-        let left = d6();
-        let right = d6();
-        while (left === right) {
-            right = d6();
-        }
-        return { left, right };
-    };
-
-    const { left: initialLeft, right: initialRight } = generateDifferentDice();
-    const [leftDie, setLeftDie] = useState<number>(initialLeft);
-    const [rightDie, setRightDie] = useState<number>(initialRight);
-    const rollLeftDie = () => {
-        let newLeft;
-        do {
-            newLeft = d6();
-        } while (newLeft === rightDie);
-        setLeftDie(newLeft);
-    };
-    const rollRightDie = () => {
-        let newRight;
-        do {
-            newRight = d6();
-        } while (newRight === leftDie);
-        setRightDie(newRight);
-    };
-
-    const gameStatus = () => {
-        if (leftDie === 1 && rightDie === 1) {
-            return "Lose";
-        } else if (leftDie === rightDie) {
-            return "Win";
-        } else {
-            return null;
-        }
-    };
-
-    const statusMessage = gameStatus();
+export function TwoDice(): JSX.Element {
+    const [d1, setD1] = useState<number>(1);
+    const [d2, setD2] = useState<number>(5);
 
     return (
         <div>
-            <div>
-                <span data-testid="left-die">{leftDie}</span>
-                <span> vs </span>
-                <span data-testid="right-die">{rightDie}</span>
-            </div>
-
-            <Button onClick={rollLeftDie}>Roll Left</Button>
-            <Button onClick={rollRightDie}>Roll Right</Button>
-
-            {statusMessage && <div>{statusMessage}</div>}
+            <Button onClick={() => setD1(d6())}>Roll Left</Button>
+            <Button onClick={() => setD2(d6())}>Roll Right</Button>
+            <br></br>
+            <span data-testid="left-die">{d1}</span>
+            <br></br>
+            <span data-testid="right-die">{d2}</span>
+            <br></br>
+            {d1 === 1 && d2 === 1 ?
+                <span>Lose</span>
+            : d1 === d2 && d1 !== 1 ?
+                <span>Win</span>
+            :   <span>Not Equal</span>}
         </div>
     );
 }
